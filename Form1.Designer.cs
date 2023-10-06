0a71daa2dae8baa0b353923140be4aa82a318c75
stinahuss <stina.huss@hotmail.com>
2023-10-06 14:10:56 +0200

    glömde fil

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -82,7 +82,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(749, 451);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
